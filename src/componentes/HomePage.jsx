9393cfe6856d3003b30fb19ffc48eeb99218cc0f
FrancoBanach <francobanach.silicon@gmail.com>
2024-11-13 22:49:48 -0300

    clinica jorge

--- src/componentes/HomePage.jsx
+++ src/componentes/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <div className="container">
       <main className="main-content">
-        <h1 className="centrado">CLINICA DE LA SALUD</h1>
+        <h1 className="centrado">CLINICA DE LA SALUD de jorge</h1>
         <h2 className="centrado">BIENVENIDOS</h2>
         <p>Ofrecemos los mejores servicios médicos para UD. y la COMUNIDAD</p>
       </main>
