3078f47ddad6f01ea37f6af90c44f87cca2e0e08
FrancoBanach <francobanach.silicon@gmail.com>
2024-11-25 20:33:19 -0300

    Add files via upload

---
+++ src/componentes/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Dashboard.css";
+
+export default function Dashboard() {
+  return (
+    <div className='Titulo-Dashboard' style={{fontFamily:"bold 18px"}}>
+      Bienvenido a Clinica de la Salud.
+    </div>
+  )
+}
